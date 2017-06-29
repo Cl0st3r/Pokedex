788502986e2c5ebe8fab6e388d3f5f790093d539
jumpingscript <jumpingscript@noreply.users.github.com>
2017-06-29 14:08:33 +0200

    Theoretical Working Schwaechen-Reading implemented.

--- Pokedex/Detailview.cs
+++ Pokedex/Detailview.cs
@@ -53,6 +53,8 @@ namespace Pokedex
             Category.Text = Convert.ToString(setValue(rd[++i]));
             Gender.Text = Convert.ToString(setValue(rd[++i]));
             Ability.Text = Convert.ToString(setValue(rd[++i]));
+            rd.Close();
+            rd = null;
         }
 
         private void schreibeTypenNameninGUI()
@@ -100,12 +102,74 @@ namespace Pokedex
 
         private void setzeTaktik()
         {
-            for(int i = 0; i < splittedValue.Length; i++)
-            {
-                
-            }            
+            com.CommandText = "SELECT Schwaechen FROM Pokemon WHERE ID = PokeID";
+            com.Parameters.AddWithValue("PokeID", PokemonID);
+            rd = com.ExecuteReader();
+            rd.Read();
+            splittedValue = splitValue(setValue(rd[0]));
+            rd.Close();
+            rd = null;
+            com.CommandText = null;
+            com.Parameters.Clear();
+
+            schreibeSchwaechenInGUI();
         }
 
+        private void schreibeSchwaechenInGUI()
+        {
+            for (int i = 0; i < splittedValue.Length; i++)
+            {
+                com.CommandText = "SELECT TypName FROM Typen WHERE ID = TypID";
+                com.Parameters.AddWithValue("TypID", splittedValue[i]);
+                rd = com.ExecuteReader();
+                rd.Read();
+                if (!String.IsNullOrEmpty(Schwaeche1.Text))
+                {
+                    if (!String.IsNullOrEmpty(Schwaeche2.Text))
+                    {
+                        if (!String.IsNullOrEmpty(Schwaeche3.Text))
+                        {
+                            if (!String.IsNullOrEmpty(Schwaeche4.Text))
+                            {
+                                if (!String.IsNullOrEmpty(Schwaeche5.Text))
+                                {
+                                    if (!string.IsNullOrEmpty(Schwaeche6.Text))
+                                    {
+
+                                    }
+                                    else
+                                    {
+                                        Schwaeche6.Text = Convert.ToString(setValue(rd[0]));
+                                    }
+                                }
+                                else
+                                {
+                                    Schwaeche5.Text = Convert.ToString(setValue(rd[0]));
+                                }
+                            }
+                            else
+                            {
+                                Schwaeche4.Text = Convert.ToString(setValue(rd[0]));
+                            }
+                        }
+                        else
+                        {
+                            Schwaeche3.Text = Convert.ToString(setValue(rd[0]));
+                        }
+                    }
+                    else
+                    {
+                        Schwaeche2.Text = Convert.ToString(setValue(rd[0]));
+                    }
+                }
+                else
+                {
+                    Schwaeche1.Text = Convert.ToString(setValue(rd[0]));
+                }
+                com.Parameters.Clear();
+                com.CommandText = null;
+            }
+        }
 
         private void aufbaueVerbindung()
         {
